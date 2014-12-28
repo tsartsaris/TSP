a54a1e7091ef0963001ec12cd0ee099b101c7545
tsartsaris <tsartsaris@gmail.com>
2014-12-28 23:14:54 +0200

    After changed the TSP parser added the plot insode the Tkinter frame

--- gui_tsp.py
+++ gui_tsp.py
@@ -14,7 +14,13 @@ from Tkinter import *
 import tkFileDialog
 import ttk
 
-from parser import *
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+from tsp_parser import *
+
+matplotlib.use('TkAgg')
 
 
 root = Tk()
@@ -24,8 +30,22 @@ root.geometry("1024x768")
 
 def openfile():
     filename = tkFileDialog.askopenfilename()
-    produce_final(filename)
+    newtsp = TSPParser(filename)
 
+    f = Figure(figsize=(8, 6), dpi=100)
+    a = f.add_subplot(111)
+
+    a.scatter(*zip(*newtsp.city_tour_tuples))
+    a.plot(*zip(*newtsp.city_tour_tuples))
+    a.set_title('Current tour plot')
+    a.set_xlabel('X axis coordinates')
+    a.set_ylabel('Y axis coordinates')
+
+
+    # a tk.DrawingArea
+    canvas = FigureCanvasTkAgg(f, master=root)
+    canvas.show()
+    canvas.get_tk_widget().grid(row=1, column=1, sticky=W)
 # entry_1 = Entry(root)
 # entry_2 = Entry(root)
 
@@ -44,13 +64,13 @@ def openfile():
 # c = Checkbutton(root, text = "keep me in man")
 # c.grid(columnspan = 2)
 
-button1 = ttk.Button(root, text = 'test', padding = 5, command = openFile)
+button1 = ttk.Button(root, text='Open TSP file', padding=5, command=openfile)
 # button1.bind("<Button-1>", printName)
 # button2 = ttk.Button(topFrame, text = 'test')
 # button3 = Button(root, text = 'test')
 # button4 = ttk.Button(bottomFrame, text = 'test')
 
-button1.grid(row = 0, column = 1, sticky = W)
+button1.grid(row=0, column=1, sticky=W)
 # button2.pack(side = LEFT)
 # button3.pack(fill = X)
 # button4.pack(side = LEFT)
---
+++ tsp_parser.py
@@ -0,0 +1,125 @@
+# ! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Tsartsaris Sotiris"
+__copyright__ = "Copyright 2014, The TSP Project"
+__credits__ = ["Tsartsaris Sotiris"]
+__license__ = "APACHE 2.0"
+__version__ = "1.0.1"
+__maintainer__ = "Tsartsaris Sotiris"
+__email__ = "info@tsartsaris.gr"
+__status__ = "Development"
+
+"""
+    With this parser we check for a file if is TSP file
+    and the we input the data for later usage. As an add
+    we plot the current instance of tour to have a visual
+    output of the data we have.
+"""
+
+
+class TSPParser:
+    def __init__(self, filename):
+        self.city_coords = {}
+        self.city_tour_init = []
+        self.city_tour_tuples = []
+        self.filename = filename
+        if self.check_filename(filename):
+            content = self.read_filename(filename)
+            self.dimension = self.get_dimension(content)
+            if self.check_dimension(self.filename, self.dimension):
+                self.edge_weight_type = self.get_edge_weight_type(self.content)
+                self.city_coords = self.get_city_coord(self.content)
+                self.city_tour_init = self.create_initial_tour()
+                self.city_tour_tuples = self.create_initial_coord_tuples()
+
+            else:
+                quit()
+        else:
+            quit()
+
+    def check_filename(self, filename):
+        """
+            Check if the file provided is a valid TSP data file
+            ...ends with .tsp
+        """
+        if self.filename.endswith(".tsp"):
+            return True
+        else:
+            print "This is not a valid TSP file"
+            return False
+
+
+    def read_filename(self, filename):
+        """
+            Read the TSP file line by line in a list
+        """
+        with open(self.filename) as f:
+            self.content = f.read().splitlines()
+        return self.content
+
+    def get_dimension(self, content):
+        """
+            Check for the line DIMENSION and return the number
+        """
+
+        for line in self.content:
+            if line.startswith("DIMENSION"):
+                index, space, rest = line.partition(':')
+                return rest.strip()
+
+    def check_dimension(self, filename, dimension):
+        """
+            Checks if the dimension found in the TSP data matches
+            the name of the file provided. ex. if you provide a file
+            named "eil101.tsp" the dimension in the file should be "101"
+        """
+        self.dimension = dimension
+        self.filename = filename
+        if self.dimension in self.filename:
+            return True
+        else:
+            print """Dimension of the file do not match with the name of the file!
+            Please make sure you have a valid TSP data file. Exiting"""
+            return False
+
+    def get_edge_weight_type(self, content):
+        """
+            Check for TSP type and return it (GEO, EUC_2D)
+        """
+        for line in self.content:
+            if line.startswith("EDGE_WEIGHT_TYPE"):
+                index, space, rest = line.partition(':')
+                return rest.strip()
+
+    def get_city_coord(self, content):
+        """
+            Returns the cities with their coordinates in a dict
+            like {'24': ('11020', '13688'), '25': ('8468', '11136'),...
+        """
+        start = self.content.index("NODE_COORD_SECTION")
+        end = self.content.index("EOF")
+        for line in self.content[start + 1:end]:
+            line = line.strip()
+            city, space, coord = line.partition(" ")
+            coord = coord.strip()
+            x, space, y = coord.partition(" ")
+            self.city_coords[int(city)] = (x.strip(), y.strip())
+        return self.city_coords
+
+    def create_initial_tour(self):
+        for i in range(1, int(self.dimension) + 1):
+            self.city_tour_init.append(i)
+        return self.city_tour_init
+
+    def create_initial_coord_tuples(self):
+        city_tour_init = self.city_tour_init
+        print city_tour_init
+        content = self.city_coords
+        for i in city_tour_init:
+            self.city_tour_tuples.append(content.get(i))
+        return self.city_tour_tuples
+
+
+if __name__ == '__main__':
+    newtsp = TSPParser("TSP_Problems/berlin52.tsp")
